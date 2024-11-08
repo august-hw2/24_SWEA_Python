c43b7445fdfd006e6c4040cd63d71300c8498240
august <55475872+august-hw2@users.noreply.github.com>
2024-11-08 23:07:50 +0900

    [D2] Title: +=, Time: 208 ms, Memory: 48,140 KB -BaekjoonHub

---
+++ SWEA/D2/21425. ＋＝/＋＝.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(1, t+1):
+    a, b, n = map(int, input().split())
+    cnt = 0
+    while(a <= n and b <= n):
+        if a < b:
+            a += b
+        else:
+            b += a
+        cnt += 1
+    print(cnt)
